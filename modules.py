97fe7d17adbe9deae78a80c86c00c1d8bfd97a40
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-03-15 21:15:21 -0500

    V4

--- modules.py
+++ modules.py
@@ -1,7 +1,8 @@
 import math
-print
-num = int(input("Enter an integer:"))
-resultado = math.log2(num)
-print(f"The log base 2 of {num} is: {resultado}")
-print(f"Floor: {math.floor(resultado)}")
-print(f"Ceiling: {math.ceil(resultado)}")
+a = int(input("Enter an integer: "))
+b = math.log2(a)
+print("Log base 2 of ",a," is: ",b)
+c = math.floor(b)
+d = math.ceil(b)
+print("Floor: ",c)
+print("Ceiling: ",d)
