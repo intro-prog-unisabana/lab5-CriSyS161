34ef0eaffce846809b1dd4c1f4f1da7a7d8c3c03
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-03-15 20:59:26 -0500

    V3

--- return_types.py
+++ return_types.py
@@ -1,4 +1,3 @@
 def obtener_precio_usuario():
-    a = input("Enter the item's price:\n")
-    return float(a)
-print(obtener_precio_usuario())
+    a = float(input("Enter the item's price:\n"))
+    return (a)
