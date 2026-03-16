7feab8bac6d54d84bdcd8f3d814492af36afb4b2
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-03-15 21:26:29 -0500

    V5

--- modules.py
+++ modules.py
@@ -1,8 +1,8 @@
 import math
 a = int(input("Enter an integer: "))
 b = math.log2(a)
-print("Log base 2 of ",a," is: ",b)
+print("Log base 2 of",a,"is:",b)
 c = math.floor(b)
 d = math.ceil(b)
-print("Floor: ",c)
-print("Ceiling: ",d)
+print("Floor:",c)
+print("Ceiling:",d)
